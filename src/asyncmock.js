c1b961436bed7ab43bfc7196d7fe6e46f5a0c530
Martin <martinchoyx@gmail.com>
2023-10-25 15:26:16 -0300

    img agregados, cambio de info en detailproduct, imgadicionales agregadas en detailproduct

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,16 +1,16 @@
 export const voidProducts = [
-    { id: 1, nombre: "HOODIE ORBITA", precio: 45, info: "Una buzo con capucha de alta calidad que te mantendrá abrigado y con estilo. Confeccionada con materiales suaves y duraderos para un confort duradero. Perfecta para cualquier ocasión", img: "/image/2.jpg" },
-    { id: 2, nombre: "LUDACRIS BROWN", precio: 20, info: "BEST HOODIE AWAY", img: "/image/3.jpg" },
-    { id: 3, nombre: "LUDACRIS BEIGE", precio: 20, info: "BEST HOODIE AWAY", img: "/image/4.jpg" },
-    { id: 4, nombre: "GATIVIDEO", precio: 35, info: "BEST HOODIE AWAY", img: "/image/5.jpg" },
-    { id: 5, nombre: "HOODIE ORBITA 2.0.1", precio: 50, info: "BEST HOODIE AWAY", img: "/image/6.jpg" },
-    { id: 6, nombre: "ZERO", precio: 35, info: "BEST HOODIE AWAY", img: "/image/7.jpg" },
-    { id: 7, nombre: "SNOW GARMENT", precio: 35, info: "BEST HOODIE AWAY", img: "/image/8.jpg" },
-    { id: 8, nombre: "GILLEL", precio: 15, info: "BEST HOODIE AWAY", img: "/image/9.jpg" },
-    { id: 9, nombre: "BROWN HUMO", precio: 15, info: "Bermuda de gabardina color gris con cintura elástica , cordón y bolsillos cargo laterales Todos los modelos pasan por un proceso industrial de lavado. Todas las fotos publicadas en nuestra web son reales, pero tene en cuenta que fueron tomadas en un estudio con flashes y cámaras profesionales", img: "/image/10.jpg" },
-    { id: 10, nombre: "OVERSHIRT LENA", precio: 22, info: "BEST HOODIE AWAY", img: "/image/11.jpg" },
-    { id: 11, nombre: "BZO GERO", precio: 10, info: "BEST HOODIE AWAY", img: "/image/12.jpg" },
-    { id: 12, nombre: "LIKE HH306", precio: 60, info: "BEST HOODIE AWAY", img: "/image/13.jpg" },
+    { id: 1, nombre: "HOODIE ORBITA", precio: 45, imgAdditional: ["/image/2.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: " • Una buzo con capucha de alta calidad que te mantendrá abrigado y con estilo.  • Confeccionada con materiales suaves y duraderos para un confort duradero.  • Perfecta para cualquier ocasión", img: "/image/2.jpg" },
+    { id: 2, nombre: "LUDACRIS BROWN", precio: 20, imgAdditional: ["/image/3.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/3.jpg" },
+    { id: 3, nombre: "LUDACRIS BEIGE", precio: 20, imgAdditional: ["/image/4.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/4.jpg" },
+    { id: 4, nombre: "GATIVIDEO", precio: 35, imgAdditional: ["/image/5.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/5.jpg" },
+    { id: 5, nombre: "HOODIE ORBITA 2.0.1", precio: 50, imgAdditional: ["/image/6.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/6.jpg" },
+    { id: 6, nombre: "ZERO", precio: 35, imgAdditional: ["/image/7.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/7.jpg" },
+    { id: 7, nombre: "SNOW GARMENT", precio: 35, imgAdditional: ["image/8.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/8.jpg" },
+    { id: 8, nombre: "GILLEL", precio: 15, imgAdditional: ["/image/9.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/9.jpg" },
+    { id: 9, nombre: "BROWN HUMO", precio: 15, imgAdditional: ["/image/10.jpg", "/image/10-2.jpg", "/image/10-3.jpg", "/image/guiadetalles.jpg"], info: "Bermuda de gabardina color gris con cintura elástica , cordón y bolsillos cargo laterales Todos los modelos pasan por un proceso industrial de lavado. Todas las fotos publicadas en nuestra web son reales, pero tene en cuenta que fueron tomadas en un estudio con flashes y cámaras profesionales", img: "/image/10.jpg" },
+    { id: 10, nombre: "OVERSHIRT LENA", precio: 22, imgAdditional: ["/image/11.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/11.jpg" },
+    { id: 11, nombre: "BZO GERO", precio: 10, imgAdditional: ["/image/12.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/12.jpg" },
+    { id: 12, nombre: "LIKE HH306", precio: 60, imgAdditional: ["/image/13.jpg", "/image/4.jpg", "/image/5.jpg", "/image/guiadetalles.jpg"], info: "BEST HOODIE AWAY", img: "/image/13.jpg" },
 ]
 
 export const getProducts = () => {
--- src/components/DetailProduct/DetailProduct.jsx
+++ src/components/DetailProduct/DetailProduct.jsx
@@ -5,9 +5,11 @@ import "./DetailProduct.css";
 import Footer from '../Footer/Footer';
 
 const DetailProduct = () => {
-  let { id } = useParams(); 
+
+  let { id } = useParams();
   const producto = voidProducts.find(item => item.id === parseInt(id, 10));
   const [contador, setContador] = useState(1);
+  const [imagenPrincipal, setImagenPrincipal] = useState(producto.img);
 
   const restarProducto = () => {
     if (contador > 1) {
@@ -20,47 +22,69 @@ const DetailProduct = () => {
       setContador(contador + 1);
     }
   };
-  
+
+  const cambiarImagenPrincipal = (nuevaImagen) => {
+    setImagenPrincipal(nuevaImagen);
+  }
+
   if (!producto) {
     return <div>Producto no encontrado</div>;
   }
 
   return (
-    <div className='detail-allcontainer'>
-      <div className='detail-container'>
-        <img src={producto.img} alt={producto.nombre} />
-        <div className='detailinfo-container'>
-          <h2>{producto.nombre}</h2>   
-          <h6>{producto.info}</h6>
-          <div className='line'></div>
-          <div className='price-container'>
-            <p>${producto.precio}</p>
-            <p>Price</p>
-          </div>
-          <div className='line'></div>
-          <div className='size-container'>
-            <h3>Size:</h3>
-            <div className='size'>
-              <button>S</button>
-              <button>M</button>
-              <button>L</button>
-              <button>XL</button>
+    
+    <div>
+        <div className='detail-allcontainer'>
+          <div className='detail-container'>
+            <div className='additional-images'>
+              {producto.imgAdditional.map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+                  alt={`Additional Image ${index + 1}`}
+                  onClick={() => cambiarImagenPrincipal(img)}
+                />
+              ))}
+            </div>
+            <img src={imagenPrincipal} alt={producto.nombre} />
+            <div className='detailinfo-container'>
+              <h2>{producto.nombre}</h2>
+              <div className='price-container'>
+                <p>${producto.precio}</p>
+                <p>Price</p>
+              </div>
+              <div className='line'></div>
+              <div className='size-container'>
+                <h3>Size:</h3>
+                <div className='size'>
+                  <button>S</button>
+                  <button>M</button>
+                  <button>L</button>
+                  <button>XL</button>
+                  <button>XXL</button>
+                </div>
+              </div>
+              <div className='counter'>
+                <button onClick={restarProducto}>-</button>
+                <span>{contador}</span>
+                <button onClick={sumarProducto}>+</button>
+              </div>
+              <button className='buy-button'>Comprar</button>
+              <div className='size-guia'>
+                <h2>Guía de talles:</h2>
+                <p>Podras encontrar la guía de talles en la última foto</p>
+              </div>
+              <h6>
+                {producto.info.split('.').map((sentence, index) => (
+                  <div key={index}>{sentence.trim()}.</div>
+                ))}
+              </h6>
             </div>
           </div>
-          <div className='counter'>
-            <button onClick={restarProducto}>-</button>
-            <span>{contador}</span>
-            <button onClick={sumarProducto}>+</button>
-          </div>
-          <button className='buy-button'>Comprar</button>
-          <div className='size-guia'>
-            <h2>Guía de talles:</h2>
-            <p>Podras encontrar la guía de talles en la última foto</p>
-          </div>
-        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
+
   );
 };
 
