a243c432872e0572dcf434263878d78df070b0a4
Martin <martinchoyx@gmail.com>
2023-10-24 23:36:37 -0300

    Mas DetailProduct agregado, position fixed en detailproduct agregado

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,16 +1,16 @@
 export const voidProducts = [
-    { id: 1, nombre: "HOODIE ORBITA", precio: 45, info: "BEST HOODIE AWAY", img: "/image/2.jpg" },
-    { id: 2, nombre: "PANTALON LUDACRIS BROWN", precio: 20, info: "BEST HOODIE AWAY", img: "/image/3.jpg" },
-    { id: 3, nombre: "PANTALON LUDACRIS BEIGE", precio: 20, info: "BEST HOODIE AWAY", img: "/image/4.jpg" },
-    { id: 4, nombre: "REMERA GATIVIDEO", precio: 35, info: "BEST HOODIE AWAY", img: "/image/5.jpg" },
+    { id: 1, nombre: "HOODIE ORBITA", precio: 45, info: "Una buzo con capucha de alta calidad que te mantendrá abrigado y con estilo. Confeccionada con materiales suaves y duraderos para un confort duradero. Perfecta para cualquier ocasión", img: "/image/2.jpg" },
+    { id: 2, nombre: "LUDACRIS BROWN", precio: 20, info: "BEST HOODIE AWAY", img: "/image/3.jpg" },
+    { id: 3, nombre: "LUDACRIS BEIGE", precio: 20, info: "BEST HOODIE AWAY", img: "/image/4.jpg" },
+    { id: 4, nombre: "GATIVIDEO", precio: 35, info: "BEST HOODIE AWAY", img: "/image/5.jpg" },
     { id: 5, nombre: "HOODIE ORBITA 2.0.1", precio: 50, info: "BEST HOODIE AWAY", img: "/image/6.jpg" },
-    { id: 6, nombre: "REMERA ZERO", precio: 35, info: "BEST HOODIE AWAY", img: "/image/7.jpg" },
-    { id: 7, nombre: "REMERA SNOW GARMENT", precio: 35, info: "BEST HOODIE AWAY", img: "/image/8.jpg" },
-    { id: 8, nombre: "REMERA GILLEL", precio: 15, info: "BEST HOODIE AWAY", img: "/image/9.jpg" },
-    { id: 9, nombre: "REMERA BROWN HUMO", precio: 15, info: "BEST HOODIE AWAY", img: "/image/10.jpg" },
+    { id: 6, nombre: "ZERO", precio: 35, info: "BEST HOODIE AWAY", img: "/image/7.jpg" },
+    { id: 7, nombre: "SNOW GARMENT", precio: 35, info: "BEST HOODIE AWAY", img: "/image/8.jpg" },
+    { id: 8, nombre: "GILLEL", precio: 15, info: "BEST HOODIE AWAY", img: "/image/9.jpg" },
+    { id: 9, nombre: "BROWN HUMO", precio: 15, info: "Bermuda de gabardina color gris con cintura elástica , cordón y bolsillos cargo laterales Todos los modelos pasan por un proceso industrial de lavado. Todas las fotos publicadas en nuestra web son reales, pero tene en cuenta que fueron tomadas en un estudio con flashes y cámaras profesionales", img: "/image/10.jpg" },
     { id: 10, nombre: "OVERSHIRT LENA", precio: 22, info: "BEST HOODIE AWAY", img: "/image/11.jpg" },
     { id: 11, nombre: "BZO GERO", precio: 10, info: "BEST HOODIE AWAY", img: "/image/12.jpg" },
-    { id: 12, nombre: "PANTALON LINE 06", precio: 25, info: "BEST HOODIE AWAY", img: "/image/13.jpg" },
+    { id: 12, nombre: "LIKE HH306", precio: 60, info: "BEST HOODIE AWAY", img: "/image/13.jpg" },
 ]
 
 export const getProducts = () => {
--- src/components/DetailProduct/DetailProduct.jsx
+++ src/components/DetailProduct/DetailProduct.jsx
@@ -1,24 +1,65 @@
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { voidProducts } from '../../asyncmock';
-import "./DetailProduct.css"
+import "./DetailProduct.css";
+import Footer from '../Footer/Footer';
 
 const DetailProduct = () => {
   let { id } = useParams(); 
   const producto = voidProducts.find(item => item.id === parseInt(id, 10));
+  const [contador, setContador] = useState(1);
 
+  const restarProducto = () => {
+    if (contador > 1) {
+      setContador(contador - 1);
+    }
+  };
+
+  const sumarProducto = () => {
+    if (contador < 10) {
+      setContador(contador + 1);
+    }
+  };
+  
   if (!producto) {
     return <div>Producto no encontrado</div>;
   }
 
   return (
-    <div className='detail-container'>
+    <div className='detail-allcontainer'>
+      <div className='detail-container'>
         <img src={producto.img} alt={producto.nombre} />
         <div className='detailinfo-container'>
-            <h2>{producto.nombre}</h2>   
-            <h6>{producto.info}</h6>
+          <h2>{producto.nombre}</h2>   
+          <h6>{producto.info}</h6>
+          <div className='line'></div>
+          <div className='price-container'>
             <p>${producto.precio}</p>
-            <button>Comprar</button>
+            <p>Price</p>
+          </div>
+          <div className='line'></div>
+          <div className='size-container'>
+            <h3>Size:</h3>
+            <div className='size'>
+              <button>S</button>
+              <button>M</button>
+              <button>L</button>
+              <button>XL</button>
+            </div>
+          </div>
+          <div className='counter'>
+            <button onClick={restarProducto}>-</button>
+            <span>{contador}</span>
+            <button onClick={sumarProducto}>+</button>
+          </div>
+          <button className='buy-button'>Comprar</button>
+          <div className='size-guia'>
+            <h2>Guía de talles:</h2>
+            <p>Podras encontrar la guía de talles en la última foto</p>
+          </div>
         </div>
+      </div>
+      <Footer/>
     </div>
   );
 };
